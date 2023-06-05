7b7262308e775145bd2ac4a6f40286d370dee482
grayuri <bv6778145@gmail.com>
2023-06-05 20:43:19 -0300

    First commit

---
+++ scripts/locale.js
@@ -0,0 +1,28 @@
+const state = document.getElementById('state');
+const city = document.getElementById('city');
+
+state.addEventListener('change', async () => {
+  const citiesUrl = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state.value}/municipios`;
+  const request = await fetch(citiesUrl);
+  const response = await request.json();
+
+  let options = '<option>...</option>';
+  response.forEach(city => {
+    options += `<option>${city.nome}</option>`;
+  });
+  
+  city.innerHTML = options;
+})
+
+window.addEventListener('load', async () => {
+  const statesUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+  const request = await fetch(statesUrl);
+  const response = await request.json();
+
+  let options = '<option>...</option>';
+  response.forEach(state => {
+    options += `<option>${state.sigla}</option>`;
+  });
+
+  state.innerHTML = options;
+});
