07fd2e3d1393756b3c4430aecf236dcebbabb77b
grayuri <bv6778145@gmail.com>
2023-07-04 21:54:03 -0300

    Done the first steps to the end

--- scripts/main.js
+++ scripts/main.js
@@ -11,8 +11,51 @@ const inputCity = document.getElementById('city')
 const inputObservation = document.getElementById('observation')
 const globalDivForm = document.getElementById('global-div')
 const globalAfter = document.getElementById('global-after')
+const getPdfButton = document.getElementById('get-pdf')
 
-function getUserInformations() {
+// Print Page --------------------------------------------------
+
+const printPage = document.getElementById('print-page')
+const userDataInputs = document.querySelectorAll('.user-datas>p')
+const userName = userDataInputs[0]
+const userBirth = userDataInputs[1]
+const userEmail = userDataInputs[2]
+const userPhone = userDataInputs[3]
+const userState = userDataInputs[4]
+const userCity = userDataInputs[5]
+const userObjective = userDataInputs[6]
+const userStatus = document.querySelectorAll('.status>h1')
+const userWeight = userStatus[0]
+const userHeight = userStatus[1]
+
+// -------------------------------------------------------------
+
+function setUserInformations(user) {
+  const completeName = `${user.name} ${user.lastName}`
+
+  userName.textContent = completeName.toUpperCase()
+  userBirth.textContent = user.birth.toUpperCase()
+  userEmail.textContent = user.email.toUpperCase()
+  userPhone.textContent = user.phone.toUpperCase()
+  userState.textContent = user.state.toUpperCase()
+  userCity.textContent = user.city.toUpperCase()
+  userWeight.textContent = user.weight
+  userHeight.textContent = user.height
+  userObjective.textContent = user.observation
+}
+
+function showAfterSection() {
+  globalDivForm.classList.add('hidden')
+  globalAfter.classList.remove('hidden')
+}
+
+function addClassToPrint() {
+  printPage.classList.add('toPrint')
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  
   const userInformations = {
     name: inputName.value,
     lastName: inputLastName.value,
@@ -26,17 +69,9 @@ function getUserInformations() {
     observation: inputObservation.value,
   }
 
-  return console.log(userInformations)
-}
-
-function showAfterSection() {
-  globalDivForm.classList.add('hidden')
-  globalAfter.classList.remove('hidden')
-}
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  getUserInformations()
+  setUserInformations(userInformations)
   showAfterSection()
-  window.location.href = '../after.html'
+  addClassToPrint()
+  
+  getPdfButton.addEventListener('click', () => window.print())
 })
